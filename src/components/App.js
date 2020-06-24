3801a7d651f132bb7e96a1d7879499917f9ade3d
kevin.garay <kevingcfcb88@gmail.com>
2020-06-24 00:19:07 -0600

    Working with class components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
 
-class App extends React.Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+const App = () => {
+  const [resource, setResource] = useState('posts');
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
+      </div>
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
 
 export default App;
